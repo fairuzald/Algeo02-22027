7717029d4acafd6295c5f0c5e2805feddfbad792
randyver <randyverdian99@gmail.com>
2023-11-08 20:14:21 +0700

    fix: change rgb to hsv and compute global in order to compare images faster

--- src/app/cbir-color/cbir-color.py
+++ src/app/cbir-color/cbir-color.py
@@ -1,82 +1,38 @@
 from PIL import Image
 import numpy as np
+import time
+from skimage.color import rgb2hsv
 
 def rgb_to_hsv(r, g, b):
-    # konversi RGB ke rentang 0-1
     r, g, b = r / 255.0, g / 255.0, b / 255.0
-    cmax, cmin = max(r, max(g, b)), min(r, min(g, b))
-    diff = cmax - cmin
-    
-    # hitung nilai HSV
-    if cmax == cmin:
-        h = 0
-    elif cmax == r:
-        h = 60 * ((g - b) / diff % 6)
-    elif cmax == g:
-        h = 60 * ((b - r) / diff + 2)
-    elif cmax == b:
-        h = 60 * ((r - g) / diff + 4)
+    cmax = np.maximum(np.maximum(r, g), b)
+    cmin = np.minimum(np.minimum(r, g), b)
+    diff = cmax - cmin + np.finfo(float).eps  # add a small constant to avoid division by zero
 
-    if cmax == 0:
-        s = 0
-    else:
-        s = diff / cmax
-    
+    h = np.where(cmax == cmin, 0,
+                 np.where(cmax == r, (60 * ((g - b) / diff % 6)),
+                          np.where(cmax == g, (60 * ((b - r) / diff + 2)),
+                                   (60 * (((r - g) / diff) + 4)))))
+
+    s = np.where(cmax == 0, 0, diff / (cmax + np.finfo(float).eps))
     v = cmax
 
     return h, s, v
 
 def compute_global_color_histogram_hsv(image, num_bins=8):
-    width, height = image.size
-    hist = np.zeros((8, 3, 3))
+    # konversi image ke NumPy array
+    image_np = np.array(image)
 
-    for y in range(height):
-        for x in range(width):
-            # nilai piksel dalam tuple RGB
-            pixel = image.getpixel((x, y))
-            
-            # konversi RGB ke HSV
-            hsv = rgb_to_hsv(*pixel[:3])
+    # konversi RGB ke HSV
+    hsv = np.stack(rgb_to_hsv(image_np[..., 0], image_np[..., 1], image_np[..., 2]), axis=-1)
 
-            # initialize bin_s, bin_h, bin_v with default values
-            bin_s, bin_h, bin_v = 0, 0, 0
+    # define bin edges
+    h_bins = [0, 1, 25.5, 40.5, 120.5, 190.5, 270.5, 295.5, 316.5, 360]
+    s_bins = [0, 0.2, 0.7, 1]
+    v_bins = [0, 0.2, 0.7, 1]
 
-            # hitung bin untuk komponen H
-            if 1 <= hsv[0] < 25.5:
-                bin_h = 1
-            elif 25.5 <= hsv[0] < 40.5:
-                bin_h = 2
-            elif 40.5 <= hsv[0] < 120.5:
-                bin_h = 3
-            elif 120.5 <= hsv[0] < 190.5:
-                bin_h = 4
-            elif 190.5 <= hsv[0] < 270.5:
-                bin_h = 5
-            elif 270.5 <= hsv[0] < 295.5:
-                bin_h = 6
-            elif 295.5 <= hsv[0] < 316.5:
-                bin_h = 7
-            elif 316.5 <= hsv[0] < 360:
-                bin_h = 0
-
-            # hitung bin untuk komponen S
-            if 0 <= hsv[1] < 0.2:
-                bin_s = 0
-            elif 0.2 <= hsv[1] < 0.7:
-                bin_s = 1
-            elif 0.7 <= hsv[1] <= 1:
-                bin_s = 2
-
-            # hitung bin untuk komponen V
-            if 0 <= hsv[2] < 0.2:
-                bin_v = 0
-            elif 0.2 <= hsv[2] < 0.7:
-                bin_v = 1
-            elif 0.7 <= hsv[2] <= 1:
-                bin_v = 2
-
-            # tambahkan ke histogram
-            hist[bin_h][bin_s][bin_v] += 1
+    # hitung histogram
+    hist, _ = np.histogramdd(hsv.reshape(-1, 3), bins=(h_bins, s_bins, v_bins))
 
     return hist
 
@@ -95,13 +51,11 @@ def compare_images(input_histogram, dataset_histograms):
 
     # Display results
     for i, similarity in enumerate(similarities):
-
-        if float(similarity) >= 0.6:
-            print(f"Similarity with image {i + 1}: {float(similarity):.2f} (Good Image)")
-        else:
-            print(f"Similarity with image {i + 1}: {float(similarity):.2f} (Not Good)")
+        print(f"Similarity with image {i + 1}: {float(similarity):.2f}")
 
 def main():
+    # inisialisasi waktu awal
+    start_time = time.time()
     # input gambar
     input_image_path = "0.jpg"
     input_image = Image.open(input_image_path)
@@ -111,7 +65,7 @@ def main():
 
     # dataset gambar
     dataset_folder_path = "img"
-    num_dataset_images = 100
+    num_dataset_images = 500
     dataset_image_paths = [f"{dataset_folder_path}/{i}.jpg" for i in range(0, num_dataset_images + 1)]
     dataset_histograms = []
 
@@ -120,8 +74,13 @@ def main():
         dataset_histogram = compute_global_color_histogram_hsv(dataset_image)
         dataset_histograms.append(dataset_histogram)
 
+
     # bandingkan input gambar dengan dataset
     compare_images(input_histogram, dataset_histograms)
+
+    # waktu yang dibutuhkan untuk seluruh perbandingan
+    total_elapsed_time = time.time() - start_time
+    print(f"Total time taken to compare all images: {total_elapsed_time:.4f} seconds")
 
 if __name__ == "__main__":
     main()
