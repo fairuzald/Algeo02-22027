9c611c190328f5aa8f7923a04d46e9e206288a17
AlthariqFairuz <althariqfairuz273@gmail.com>
2023-11-09 21:32:02 +0700

    fix : fixing minor bugs in cbirtext

--- src/app/cbir-texture/cbir-texture.py
+++ src/app/cbir-texture/cbir-texture.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+import os
 
 def convert_to_grayscale(image_path):
     # Membuka gambar
@@ -39,11 +40,13 @@ def quantize_grayscale(image_path, levels=256):
     return img_quantized
 
 def create_glcm(image, distance= 1):
-
+    # Membuat gambar menjadi array numpy
     img = np.array(image)
 
+    # Inisialisasi matriks kosong
     glcm = np.zeros((256, 256), dtype=int)
 
+    # Isi matriks
     first = img[distance:, :]
     second = img[:-distance, :]
     for i, j in zip(first.ravel(), second.ravel()):
@@ -52,11 +55,13 @@ def create_glcm(image, distance= 1):
     return glcm
 
 def normalize(matrix):
+    # Membuat matriks simetris
     symetric = matrix+matrix.T
     return symetric/symetric.sum()
 
 def calculate_contrast(matrix):
     contrast = 0
+    # Hitung kontras
     for i in range(256):
         for j in range(256):
             contrast += ((i - j) ** 2) * matrix[i][j]
@@ -64,6 +69,7 @@ def calculate_contrast(matrix):
 
 def calculate_entropy(matrix):
     entropy = 0
+    # Menghitung entropy
     for i in range(256):
         for j in range(256):
             if matrix[i][j] > 0:
@@ -72,10 +78,42 @@ def calculate_entropy(matrix):
 
 def calculate_homogeneity(matrix):
     homogeneity = 0
+    # Menghitung homogenitas
     for i in range(256):
         for j in range(256):
             homogeneity += matrix[i][j] / (1 + (i - j)**2)
     return homogeneity
 
 def cosine_similarity(vec1, vec2):
+    # Menghitung cosine similarity
     return np.dot(vec1, vec2)/(np.linalg.norm(vec1) * np.linalg.norm(vec2))
+
+def process_image(image_path, distance=1, levels=256):
+    # Mengubah gambar menjadi grayscale dan melakukan kuantisasi
+    img_quantized = quantize_grayscale(image_path, levels)
+
+    # Membuat GLCM
+    glcm = create_glcm(img_quantized, distance)
+
+    # Normalisasi matriks
+    norm_glcm = normalize(glcm)
+
+    # Menghitung kontras, entropi, dan homogenitas
+    contrast = calculate_contrast(norm_glcm)
+    entropy = calculate_entropy(norm_glcm)
+    homogeneity = calculate_homogeneity(norm_glcm)
+
+    return contrast, entropy, homogeneity
+
+contrast1, entropy1, homogeneity1 = process_image('./image/1.jpg')
+directory = './image/'
+for filename in os.listdir(directory):
+    # Baca file gambar 
+    if filename.endswith('.jpg'):
+        # Buat path file
+        filepath = os.path.join(directory, filename)
+        # Proses gambar
+        contrast2,entropy2,homogeneity2 = process_image(filepath)
+        # Hitung tingkat kemiripan
+        cosine_similarity_value = cosine_similarity([contrast1, entropy1, homogeneity1], [contrast2, entropy2, homogeneity2])
+        print('Tingkat kemiripan dengan image {}: {:.3f}'.format(filepath, cosine_similarity_value))
